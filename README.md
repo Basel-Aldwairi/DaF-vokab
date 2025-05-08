98c2b8faf04b91ca7fb982a68a8104e3c85750fb
Basel-Aldwairi <aldwairi.basel0@gmail.com>
2025-05-08 18:37:37 +0300

    txt I/O
    added multiple inputs from txt and output

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # DaF-vokab
+
 To learn Python while studying for the DaF
+
+___
+
+### Input
+
+- `.txt` file
+  - Thema
+    - German -> English
+
+___
+
+### Script
+
+- Dictionary
+  - Thema
+  - Translation List
+    -German English Tuple
+-Input -> Dict
+-Dict -> Output
+
+___
+
+### Output
+
+- `.pdf` or `.txt` file
+  - Thema
+    - German | English
---
+++ betterTranslations.py
@@ -0,0 +1,51 @@
+from time import process_time_ns
+
+from reportlab.pdfgen import canvas as ca
+
+
+def fromTxt(input: str) -> dict[str, list[tuple]]:
+    germanEnglish = dict()
+    thema = str()
+    with open(input, 'r', encoding='utf-8') as file:
+        for line in file:
+
+            if '(' in line and '*' not in line and line[0] != ' ':
+
+                index = line.index('(')
+                thema = ''.join([line[:index - 1]])
+                germanEnglish[thema] = []
+                print(f'thema {thema}')
+
+            elif '**' in line:
+                thema = ''.join([line[2:- 3]])
+                germanEnglish[thema] = []
+                print(f'thema {thema}')
+            elif '→' in line:
+                startIndex = line.index('→')
+                start = 4
+                if '*' in line:
+                    start = 2
+                german = line[start:startIndex - 1]
+                english = line[startIndex + 2:-1]
+                germanEnglish[thema].append((german, english))
+                print(f'{german} | {english}')
+    return germanEnglish
+
+
+def toTxt(output: str, dictionary: dict[str, list[set]]):
+    i = 1
+    with open(output, 'w', encoding='utf-8') as file:
+        for thema, translation in dictionary.items():
+            file.write(f'{thema} : \n')
+            file.write('\n')
+            for german, english in translation:
+                file.write(f'  {i:3} : {german:26} | {english}\n')
+                i += 1
+                print(i)
+            file.write('\n\n')
+
+
+dictionary = fromTxt('input')
+dictionary2 = fromTxt('input2')
+dic = {**dictionary, **dictionary2}
+toTxt('output', dic)
